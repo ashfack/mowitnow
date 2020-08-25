2efb30cc101d7ca4b4bcced044a31afff3e4e11f
ABDOUL-AZID <ashfack.abdoul-azid@cgi.com>
2020-08-25 12:39:44 +0200

    Spring starter project

---
+++ mow/src/main/java/com/mowitnow/mow/MowApplication.java
@@ -0,0 +1,13 @@
+package com.mowitnow.mow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MowApplication.class, args);
+	}
+
+}
